bcc111fc3d3e85171dc4123f8d7815b98f0d3287
ChewCEC <camilo.echeverri.100@gmail.com>
2022-10-16 19:32:13 -0500

    commit 2

--- futbolista.py
+++ futbolista.py
@@ -5,8 +5,8 @@ from deportista import Deportista
 class Futbolista (Persona, Deportista):
     listaFutbolistas = []
     def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
-        Persona.__init__(nombre, edad, altura, sexo)
-        Deportista.__init__("Futbol",añosPracticando)
+        Persona.__init__(self, nombre, edad, altura, sexo)
+        Deportista.__init__(self, "Futbol",añosPracticando)
         self._golesMarcados = golesMarcados
         self._tarjetasRojas = tarjetasRojas
         self._piernaHabil = piernaHabil
@@ -37,3 +37,14 @@ class Futbolista (Persona, Deportista):
     def __str__(self):
         return "Mi nombre es " + self._nombre + " soy profesional en el deporte " + self._deporte + " Tengo " + str(self._edad) + " años de edad y llevo " + str(self._añosPracticando) + " años en el deporte"
 
+
+if __name__ == "__main__":
+    def testFutbolista():
+        futbolista = Futbolista("Juan Pablo", 30, "1,80", "M", 12, 400, 1, "Derecha")
+        ok = 1
+        if (futbolista.getNombre() == "Juan Pablo" and futbolista.getEdad() == 30 and futbolista.getAltura()=="1,80" and futbolista.getSexo() == "M" and 
+            futbolista.getAñosPracticando() == 12 and futbolista.getGolesMarcados()==400 and futbolista.getTarjetasRojas() == 1 and futbolista.getPiernaHabil() == "Derecha"):
+            ok = 0
+        return ok
+print(testFutbolista())
+    
